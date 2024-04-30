8d5ba8df62efe4c8177f5e11b8e37d71e67319a7
Weber <Weber@DESKTOP-IM983HF>
2024-04-29 21:46:39 -0500

    Add project files.

---
+++ Nova/Components/Pages/Home.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.JSInterop;
+
+namespace Nova.Components.Pages
+{
+    public partial class Home
+    {
+        string recordButton = "record";
+
+        bool recording = false;
+        bool notRecording = true;
+
+
+        private async Task Record()
+        {
+
+            recordButton = "recording";
+            recording = true;
+            notRecording = false;
+            await jsRuntime.InvokeVoidAsync("MyJSMethods.startRecording");
+        }
+
+        private async Task Stop()
+        {
+            recordButton = "record";
+            recording = false;
+            notRecording = true;
+            await jsRuntime.InvokeVoidAsync("MyJSMethods.stopRecording");
+        }
+
+        private void RecordB()
+        {
+            int x = 0;
+        }
+    }
+}
---
+++ Nova/SaveAudio.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nova;
+
+public class SaveAudio: Controller
+{
+    Microsoft.AspNetCore.Hosting.IWebHostEnvironment _hostingEnvironment;
+
+    public SaveAudio(Microsoft.AspNetCore.Hosting.IWebHostEnvironment hostingEnvironment)
+    {
+        _hostingEnvironment = hostingEnvironment;
+
+    }
+
+    [Route("api/SaveAudio/Save")]
+    [HttpPost]
+    public async Task<IActionResult> Save(IFormFile file)
+    {
+        if (file.ContentType != "audio/wav")
+        {
+            return BadRequest("Wrong file type");
+        }
+        var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");//uploads where you want to save data inside wwwroot
+
+        //var filePath = Path.Combine(uploads, Path.GetRandomFileName() + ".wav");
+        var filePath = Path.Combine(uploads, file.FileName + ".wav");
+        using (var fileStream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(fileStream);
+        }
+        return Ok("File uploaded successfully");
+    }
+}
